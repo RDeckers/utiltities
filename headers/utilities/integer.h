3c650058d216303174f882e9dab08e1737a3706d
R. Deckers <github.com@codingcat.nl>
2016-05-18 17:23:34 +0200

    triangle number function

--- headers/utilities/integer.h
+++ headers/utilities/integer.h
@@ -4,13 +4,16 @@
 #ifdef __cplusplus
 extern "C"{
 #endif
-317
+
 inline unsigned iabs(int x){return x>0?x:-x;}
 inline int signOf(int x){return x<0?-1:1;}
 
 void generatePrimes(unsigned *primes, unsigned N_PRIMES);
 int isqrt(int n);
 unsigned usqrt(unsigned x); //fails on x= UINT_MAX, gives 0.
+unsigned triag_nr(unsigned x){
+  return (x*x+x)/2;
+}
 
 #ifdef __cplusplus
 }
